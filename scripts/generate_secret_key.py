05772c93ddbdefb5a2868c2ab403c76369bc1bc6
DESKTOP-5Q9K2NL\user1 <chay6865@gmail.com>
2024-09-15 21:25:48 +0300

    before fix wrongs

--- scripts/generate_secret_key.py
+++ scripts/generate_secret_key.py
@@ -1,7 +1,19 @@
 import os
 from dotenv import set_key
 
+env_path = '../.env'
+
+env_vars = {}
+if os.path.exists(env_path):
+    with open(env_path, 'r') as file:
+        for line in file:
+            if '=' in line:
+                key, _ = line.strip().split('=', 1)
+                env_vars[key] = True
 
 secret_key = os.urandom(32).hex()
-set_key('../.env', 'SECRET_KEY', secret_key)
+
+if 'SECRET_KEY' not in env_vars:
+    set_key('../.env', 'SECRET_KEY', secret_key)
+
 
